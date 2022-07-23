259ef3c6dc1c4559c595f2abd47bb2ec14e5b6df
Naz <bluspringindohq@gmail.com>
2022-07-24 04:29:05 +0800

    Welp

--- src/main/kotlin/xyz/bluspring/lifelinedeathhandler/server/integration/StreamIntegration.kt
+++ src/main/kotlin/xyz/bluspring/lifelinedeathhandler/server/integration/StreamIntegration.kt
@@ -5,6 +5,8 @@ import io.socket.client.Socket
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 import net.minecraft.util.Identifier
 import org.slf4j.Logger
 import xyz.bluspring.lifelinedeathhandler.LifelineDeathHandler
@@ -69,6 +71,9 @@ abstract class StreamIntegration(val player: ServerPlayerEntity, val apiKey: Str
     protected abstract fun onEvent(vararg args: Any)
 
     protected fun sendInvalidIntegrationError() {
-        ServerPlayNetworking.send(player, Identifier("lifelinesmp", "integration_invalid"), PacketByteBufs.empty())
+        //ServerPlayNetworking.send(player, Identifier("lifelinesmp", "integration_invalid"), PacketByteBufs.empty())
+        player.sendMessage(
+            Text.literal("Your ${integrationType.integrationName} failed to connect due to an invalid API key, please fix it!").formatted(
+            Formatting.RED))
     }
 }
